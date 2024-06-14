847015292bfe4b811e7630abfd9d381ed09e667b
Lucas Gertel <lgertel@gmail.com>
2024-06-14 13:54:54 -0300

    feat(cms-backend): initialize CMS backend project with necessary configuration and initial files
    - Add `.editorconfig` for consistent coding styles.
    - Add `.env.example` to provide an example environment configuration.
    - Add `.eslintignore` to specify files and directories to be ignored by ESLint.
    - Add `.eslintrc` for ESLint configuration.
    - Add `.gitignore` to specify files and directories to be ignored by Git.
    - Add `README.md` for project documentation.
    - Add configuration files: `admin.js`, `api.js`, `database.js`, `middlewares.js`, `plugins.js`, and `server.js` in `config`.
    - Add `.gitkeep` in `database/migrations` to track the directory in Git.
    - Add `favicon.png` for the project's favicon.
    - Add `jsconfig.json` for JavaScript configuration.
    - Add `package.json` for npm dependencies management.
    - Add `robots.txt` in `public` directory.
    - Add `.gitkeep` in `public/uploads` to track the directory in Git.
    - Add example files: `app.example.js` and `webpack.config.example.js` in `src/admin`.
    - Add `.gitkeep` in `src/api` to track the directory in Git.
    - Add API files: `schema.json`, `home-page.js` (controller, route, and service) for `home-page` in `src/api/home-page`.
    - Add `.gitkeep` in `src/extensions` to track the directory in Git.
    - Add `index.js` as the entry point of the application.
    - Add TypeScript definitions: `components.d.ts` and `contentTypes.d.ts` in `types/generated`.
    - Add `yarn.lock` for Yarn dependencies management.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,24 @@
-import { Button } from "@/components/ui/button";
+async function getStrapiData(url: string) {
+  const baseUrl = "http://localhost:1337";
+  try {
+    const response = await fetch(baseUrl + url);
+    const data = await response.json();
+    console.log(data)
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-export default function Home() {
-  console.log("Hello, NextGen Developers!");
+export default async function Home() {
+  const strapiData = await getStrapiData("/api/home-page");
+
+  const { title, description } = strapiData.data.attributes;
+
   return (
     <main className="container mx-auto py-6">
-      <Button>NextGen Developers</Button>
+      <h1 className="text-5xl font-bold">{title}</h1>
+      <p className="text-xl mt-4">{description}</p>
     </main>
   );
 }
